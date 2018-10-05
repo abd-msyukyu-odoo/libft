9a26a63f45a286a50acad92c122aaa713906ac6c
Damien Abeloos <dabeloos@w-r6-p7.s19.be>
2018-10-05 15:23:28 +0200

    ft_strcpy

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/10/04 12:40:14 by dabeloos          #+#    #+#              #
-#    Updated: 2018/10/05 15:13:57 by dabeloos         ###   ########.fr        #
+#    Updated: 2018/10/05 15:21:29 by dabeloos         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,8 @@ C_FILES		= ft_memset.c \
 			  ft_memchr.c \
 			  ft_memcmp.c \
 			  ft_strlen.c \
-			  ft_strdup.c
+			  ft_strdup.c \
+			  ft_strcpy.c
 
 O_FILES		= $(C_FILES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/04 12:57:55 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/05 15:14:13 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/05 15:22:10 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,5 @@ void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t		ft_strlen(const char *s);
 char		*ft_strdup(const char *s1);
+char		*ft_strcpy(char *dst, const char *src);
 #endif
