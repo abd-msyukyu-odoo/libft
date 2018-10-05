39b66319cde95397b228cb6bd6445777aa55e489
Damien Abeloos <dabeloos@w-r6-p7.s19.be>
2018-10-05 14:49:22 +0200

    ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,23 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/04 12:57:55 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/05 14:38:51 by dabeloos         ###   ########.fr       */
+/*   Created: 2018/10/05 14:30:08 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/10/05 14:42:06 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
+#include "libft.h"
 
-void		*ft_memset(void *b, int c, size_t len);
-void		ft_bzero(void *s, size_t n);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void		*ft_memmove(void *dst, const void *src, size_t len);
-void		*ft_memchr(const void *s, int c, size_t n);
-#endif
+void		*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+
+	i = 0;
+	d = (unsigned char *)s;
+	while (i < n)
+	{
+		if (*(d + i) == (unsigned char)c)
+			return (d + i);
+		++i;
+	}
+	return (NULL);
+}
