8eeeacf7a7186161464c86bcd5a488f08dfbd427
Damien Abeloos <dabeloos@w-r6-p7.s19.be>
2018-10-05 17:08:25 +0200

    simplified condition ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/05 16:06:27 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/05 16:53:53 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/05 17:08:09 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t		l;
 	size_t		i;
-	
+
 	l = ft_strlen(dst);
-	if (size < l + 1)
+	if (l + 1 > size)
 		return (ft_strlen(src) + size);
 	i = 0;
 	while (l + i < size - 1 && src[i] != '\0')
@@ -26,7 +26,6 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 		dst[l + i] = src[i];
 		++i;
 	}
-	if (l < size)
-		dst[l + i] = '\0';
+	dst[l + i] = '\0';
 	return (l + ft_strlen(src));
 }
