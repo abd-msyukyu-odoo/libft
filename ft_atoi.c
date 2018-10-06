c36cdcb2f8b6114ea0e5dcd135ca994e2a24d110
Damien Abeloos <dabeloos@w-r9-p1.s19.be>
2018-10-06 19:07:44 +0200

    ft_atoi finished

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/06 16:51:58 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/06 17:16:35 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/06 19:04:53 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int			ft_atoi(const char *str)
 	long		result;
 	size_t		i;
 	char		sign;
+	long		tmp;
 
 	result = 0;
 	i = 0;
@@ -27,6 +28,12 @@ int			ft_atoi(const char *str)
 	if (str[i] == '+' || (str[i] == '-' && (sign = -1)))
 		++i;
 	while (str[i] >= '0' && str[i] <= '9')
+	{
+		tmp = result;
 		result = result * 10 + sign * (str[i++] - '0');
+		if (tmp != 0 && (tmp ^ result) < 0)
+			return ((result < 0) ?
+					(int)((~((long)0)) >> 1) : (int)(~((~((long)0)) >> 1)));
+	}
 	return ((int)result);
 }
