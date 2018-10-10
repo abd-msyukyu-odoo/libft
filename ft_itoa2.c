921f9128f32e1422b9a38f05159d6dac621edd56
Damien Abeloos <dabeloos@e-r8-p4.s19.be>
2018-10-10 14:27:39 +0200

    ft_itoa2

--- ft_itoa2.c
+++ ft_itoa2.c
@@ -6,27 +6,27 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 12:49:56 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/10 13:15:23 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/10 14:26:38 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		*ft_itoa_rec(long n, size_t count)
+static char		*ft_itoa_rec(long n, size_t i)
 {
 	char	*str;
 
 	if (n < 10 && n > -10)
 	{
-		if (!(str = ft_strnew(count + ((n < 0) ? 2 : 1))))
+		if (!(str = ft_strnew(i + ((n < 0) ? 2 : 1))))
 			return (NULL);
 		if (n < 0)
-			str[count + 1] = '-';
+			str[i + 1] = '-';
 	}
 	else
-		str = ft_itoa_rec(n / 10, count + 1);
+		str = ft_itoa_rec(n / 10, i + 1);
 	if (str != NULL)
-		str[count] = ((n < 0) ? -1 : 1) * (n % 10) + '0';
+		str[i] = ((n < 0) ? -1 : 1) * (n % 10) + '0';
 	return (str);
 }
 
