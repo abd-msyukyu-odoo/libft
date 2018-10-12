94aab9629f777da20c449736ecd21ad1ecbb70d2
Damien Abeloos <dabeloos@w-r4-p4.s19.be>
2018-10-12 15:24:36 +0200

    ft_array_strdel

--- ft_array_strdel.c
+++ ft_array_strdel.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 14:40:43 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/12 15:16:04 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/12 15:24:25 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ void		ft_array_strdel(char ***arr)
 		free((*arr)[i]);
 	}
 	free(*arr);
+	*arr = NULL;
 }
