b970c7e7958196cc8374f0b282170ba2ba3b7d8e
Damien Abeloos <dabeloos@e-r11-p4.s19.be>
2018-10-17 11:43:07 +0200

    ft_strncmp rewrite

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/06 16:40:33 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/10/17 10:08:34 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/10/17 11:42:01 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (i < n && s1[i] != '\0' &&
 			(unsigned char)s1[i] - (unsigned char)s2[i] == 0)
 		++i;
+	if (i == n)
+		--i;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
