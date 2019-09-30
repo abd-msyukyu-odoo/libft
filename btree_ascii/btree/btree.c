a5703e1295d038fb96d3a4082d283718d17a6281
Damien Abeloos <dabeloos@w-r11-p3.s19.be>
2019-09-30 13:27:13 +0200

    wavl btree with rebalancing prototype

--- btree_ascii/btree/btree.c
+++ btree_ascii/btree/btree.c
@@ -82,6 +82,13 @@ t_named				*ft_btree_get(t_btree *btree, char *key)
 	return ((!target) ? NULL : target->named);
 }
 
+static int			ft_bnode_has_two_leaves(t_bnode *bnode)
+{
+	if (!bnode->left || !bnode->right)
+		return (0);
+	return (!bnode->left->rank && !bnode->right->rank);
+}
+
 static int			ft_bnode_sibling_spin(t_bnode *child, t_bnode **sibling)
 {
 	if (child->up->left == child)
@@ -118,15 +125,13 @@ static void			ft_btree_rotate(t_btree *btree, t_bnode *bn, int spin)
 	return ;
 }
 
-static void			ft_btree_rebalance(t_btree *btree, t_bnode *bn)
+static void			ft_btree_rebalance_added(t_btree *btree, t_bnode *bn)
 {
 	t_bnode			*sib;
 	int				spin;
 
-	while (bn->up)
+	while (bn->up && bn->up->rank == bn->rank)
 	{
-		if (bn->up->rank > bn->rank)
-			return ;
 		spin = ft_bnode_sibling_spin(bn, &sib);
 		if (bn->rank == bn->up->rank && bn->rank > sib->rank + 1)
 		{
@@ -146,6 +151,54 @@ static void			ft_btree_rebalance(t_btree *btree, t_bnode *bn)
 	}
 }
 
+static void			ft_btree_rebalance_deleted_double_rotate(t_btree *btree,
+	t_bnode *bn, t_bnode *sib, int spin)
+{
+	bn->up->rank--;
+	bn = (spin == 1) ? sib->left : sib->right;
+	bn->rank++;
+	ft_btree_rotate(btree, bn, spin);
+	ft_btree_rotate(btree, bn, -1 * spin);
+}
+
+static void			ft_btree_rebalance_deleted_single_rotate(t_btree *btree,
+	t_bnode *bn, t_bnode *sib, int spin)
+{
+	bn = bn->up;
+	sib->up->rank++;
+	ft_btree_rotate(btree, sib->up, -1 * spin);
+	if (ft_bnode_has_two_leaves(bn))
+		bn->rank--;
+}
+
+static void			ft_btree_rebalance_deleted(t_btree *btree, t_bnode *bn)
+{
+	t_bnode			*sib;
+	int				spin;
+
+	while (bn->up && bn->up->rank - bn->rank == 3)
+	{
+		spin = ft_bnode_sibling_spin(bn, &sib);
+		if (sib->rank - bn->rank == 2)
+		{
+			if (sib->left->rank - sib->right->rank == spin)
+			{
+				ft_btree_rebalance_deleted_double_rotate(btree, bn, sib, spin);
+				return ;
+			}
+			sib = (spin == 1) ? sib->right : sib->left;
+			if (sib->up->rank - sib->rank == 1)
+			{
+				ft_btree_rebalance_deleted_single_rotate(btree, bn, sib, spin);
+				return ;
+			}
+			sib->up->rank--;
+		}
+		bn->up->rank--;
+		bn = bn->up;
+	}
+}
+
 int					ft_btree_add(t_btree *btree, t_named *item)
 {
 	t_bnode			*cur;
@@ -159,7 +212,7 @@ int					ft_btree_add(t_btree *btree, t_named *item)
 		return (0);
 	cur->named = item;
 	cur->rank = 1;
-	ft_btree_rebalance(btree, cur);
+	ft_btree_rebalance_added(btree, cur);
 	return (1);
 }
 
@@ -203,6 +256,7 @@ static int			ft_btree_cut_branch(t_btree *btree, t_bnode *cut)
 	referent = ft_btree_bnode_referent(btree, cut);
 	remain->up = cut->up;
 	*referent = remain;
+	ft_btree_rebalance_deleted(btree, remain);
 	return (1);
 }
 
@@ -227,7 +281,6 @@ static t_bnode		*ft_btree_remove_bnode(t_btree *btree, char *key)
 		target->named = swapper->named;
 		target = swapper;
 		ft_btree_cut_branch(btree, target);
-		// need rebalancing
 	}
 	return (target);
 }
