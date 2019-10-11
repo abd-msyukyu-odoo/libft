7fffcf472ded1f44070aa5e097c636dd24ca8ced
msyukyu <damien_ab@hotmail.com>
2019-10-11 15:55:42 +0200

    memanager structures

---
+++ includes/memory_manager/memanager.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memanager.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/27 16:16:07 by dabeloos          #+#    #+#             */
+/*   Updated: 2019/09/27 16:16:08 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MEMANAGER_H
+# define MEMANAGER_H
+# include <stdlib.h>
+# include "array/array.h"
+# include "memory_manager/typemanager.h"
+# include "btree/tbtree.h"
+
+typedef struct			s_key_stbtree
+{
+	size_t				key;
+}						t_key_stbtree;
+
+typedef struct			s_stbtree
+{
+	t_key_stbtree		size;
+	t_tbtree			*addr_tbt;
+}						t_stbtree;
+
+typedef struct			s_memjump
+{
+	struct s_memjump	*next;
+	struct s_memjump	*prev;
+}						t_memjump;
+
+typedef struct			s_memanager
+{
+	t_typemanager		*tbtree_mng;
+	t_typeused			*tbtree_used;
+	t_typemanager		*tbnode_mng;
+	t_typeused			*tbnode_used;
+	t_typemanager		*stbtree_mng;
+	t_typeused			*stbtree_used;
+	t_array				*memarrays;
+	t_tbtree			*stbtree_tbt;
+}						t_memanager;
+
+#endif
