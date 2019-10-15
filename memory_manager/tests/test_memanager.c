60e6ae088506b9263e3aa747fdf29ae8b030b0b0
msyukyu <damien_ab@hotmail.com>
2019-10-15 10:03:06 +0200

    testing in progress

---
+++ memory_manager/tests/test_memanager.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "libft.h"
+
+char	*a = "a";
+char	*b = "ab";
+char	*c = "abc";
+char	*d = "abcd";
+char	*e = "abcde";
+char	letter = 'l';
+int		neg = -705456;
+size_t	size = -156;
+
