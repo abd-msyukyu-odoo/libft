2b7ec00247bcb5d61a26b893a8529152783f12cc
Damien Abeloos <dabeloos@w-r9-p1.s19.be>
2019-10-22 13:26:14 +0200

    stresstest

---
+++ memory_manager/tests/stresstest_memanager.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+#define CMP_MMNG_INSTANCES 20000
+#define CMP_MMNG_SIZES 100
+
+void			basic_memanager()
+{
+	t_memanager	*mng;
+	char		*test[CMP_MMNG_INSTANCES];
+
+	mng = ft_memanager_construct_default();
+	for (int i = 0; i < CMP_MMNG_INSTANCES; i++)
+	{
+		test[i] = ft_memanager_get(mng, sizeof(char) * (1 + i % CMP_MMNG_SIZES));
+	}
+	for (int i = 0; i < CMP_MMNG_INSTANCES; i++)
+	{
+		ft_memanager_refill(mng, test[i]);
+		test[i] = ft_memanager_get(mng, sizeof(char) * (CMP_MMNG_SIZES - i % CMP_MMNG_SIZES));
+	}
+	ft_memanager_free(mng);
+}
+
+int				main(void)
+{
+	basic_memanager();
+	return (0);
+}
