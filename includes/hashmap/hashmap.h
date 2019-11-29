ed129dd45404b09b683b3a7be72393c7a0cd6e34
Damien Abeloos <dabeloos@e-r11-p4.s19.be>
2019-11-29 16:18:56 +0100

    search and get by key hashmap

--- includes/hashmap/hashmap.h
+++ includes/hashmap/hashmap.h
@@ -17,6 +17,12 @@
 # include "array/array.h"
 # include "btree/btree.h"
 
+typedef struct			s_hmap_wrapper
+{
+	void				*item;
+	int					(*equals)(void *o1, void *o2);
+}						t_hmap_wrapper;
+
 typedef struct			s_hmap
 {
 	t_array				*array;
@@ -31,14 +37,18 @@ size_t					ft_hmap_hash_nbr(void *s, size_t size);
 int						ft_hmap_bnode_iteration(void *receiver, t_hmap *source,
 	int (*f)(void *receiver, void *sent));
 
+int						ft_hmap_cell_identification(void *receiver, void *sent);
+
 t_btree					*ft_hmap_get_cell(t_hmap *hmap, void *item);
 
 int						ft_hmap_contains(t_hmap *hmap, void *item);
 
+int						ft_hmap_contains_key(t_hmap *hmap, void *key,
+	int (*equals)(void *s1, void *s2));
+
+void					*ft_hmap_get(t_hmap *hmap, void *key,
+	int (*equals)(void *s1, void *s2));
+
 int						ft_hmap_is_empty(t_hmap *hmap);
-
-
-
-
 
 #endif
