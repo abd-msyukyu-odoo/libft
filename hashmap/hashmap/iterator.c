1bb9c2fc090da1e8a0040717fb319dfc6f2fe9f9
Damien Abeloos <dabeloos@e-r11-p4.s19.be>
2019-11-29 16:00:46 +0100

    search and get by key hmap

--- hashmap/hashmap/iterator.c
+++ hashmap/hashmap/iterator.c
@@ -17,3 +17,16 @@ int					ft_hmap_bnode_iteration(void *receiver, t_hmap *source,
 {
 	return (ft_btree_bnode_iteration(receiver, source->hash_btree->root, f));
 }
+
+int					ft_hmap_cell_identification(void *receiver, void *sent)
+{
+	t_hmap_wrapper	*wrapper;
+
+	wrapper = (t_hmap_wrapper*)receiver;
+	if (wrapper->equals(wrapper->item, sent))
+	{
+		wrapper->item = sent;
+		return (0);
+	}
+	return (1);
+}
--- hashmap/hashmap/search.c
+++ hashmap/hashmap/search.c
@@ -30,5 +30,34 @@ int					ft_hmap_contains(t_hmap *hmap, void *item)
 
 int					ft_hmap_is_empty(t_hmap *hmap)
 {
+	if (!hmap)
+		return (-1);
 	return (!hmap->hash_btree->root->rank);
+}
+
+void				*ft_hmap_get(t_hmap *hmap, void *key,
+	int (*equals)(void *s1, void *s2))
+{
+	t_hmap_wrapper	wrapper;
+	t_btree			*btree;
+
+	wrapper.item = key;
+	wrapper.equals = equals;
+	btree = ft_hmap_get_cell(hmap, key);
+	if (!btree->root)
+		wrapper.item = NULL;
+	else
+	{
+		ft_btree_bnode_iteration(&wrapper, btree->root,
+			ft_hmap_cell_identification);
+		if (wrapper.item == key)
+			wrapper.item = NULL;
+	}
+	return (wrapper.item);
+}
+
+int					ft_hmap_contains_key(t_hmap *hmap, void *key,
+	int (*equals)(void *s1, void *s2))
+{
+	return (ft_hmap_get(hmap, key, equals) != NULL);
 }
